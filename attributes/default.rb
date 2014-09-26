79a5b3a8993cc072666ba43de6a248bb93d92098
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-09-26 12:14:24 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['jvm_host']['tomcat_defaults_path'] = "/etc/default/tomcat7"
 
 default['jvm_host']['add_host_param'] = false
 default['jvm_host']['param_name'] = "host"
-default['jvm_host']['hostname'] = "#{node[:hostname]}.#{node[:domain]}"
+default['jvm_host']['hostname'] = "#{node['hostname']}.#{node['domain']}"
 
 default['term_delimiter_start'] = "@@"
 default['term_delimiter_end'] = "@@"
