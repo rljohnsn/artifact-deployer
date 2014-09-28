793d009418802107478bece7a161d4f13f69ee63
Maurizio Pillitu <maurizio@session.it>
2014-09-28 03:54:33 +0200

    fixes kitchen test for junit1

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -3,6 +3,6 @@ require 'serverspec'
 include SpecInfra::Helper::Exec
 include SpecInfra::Helper::DetectOS
 
-describe file('/home/vagrant/default_suite/junit.jar') do
+describe file('/home/vagrant/default_suite/junit1.jar') do
   it { should be_file  }
 end
