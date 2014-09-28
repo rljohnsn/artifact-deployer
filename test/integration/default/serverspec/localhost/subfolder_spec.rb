7d8068959aadc6087d5174035deb82f9b5b79433
Maurizio Pillitu <maurizio@session.it>
2014-09-28 03:45:33 +0200

    disabled kitchen exec with travis; consolidated kitchen tests into one suite

--- test/integration/default/serverspec/localhost/subfolder_spec.rb
+++ test/integration/default/serverspec/localhost/subfolder_spec.rb
@@ -3,11 +3,11 @@ require 'serverspec'
 include SpecInfra::Helper::Exec
 include SpecInfra::Helper::DetectOS
 
-describe file('/home/vagrant/subfolder_suite/junit') do
+describe file('/home/vagrant/subfolder_suite/junit3') do
   it { should be_directory  }
 end
 
-describe file('/home/vagrant/subfolder_suite/junit/org') do
+describe file('/home/vagrant/subfolder_suite/junit3/org') do
   it { should_not be_file }
   it { should_not be_directory }
 end
