21011968255324b9060f71401b7425368372cfc1
Maurizio Pillitu <maoo@apache.org>
2015-02-26 03:18:32 +0000

    install maven settings even if databags are missing

--- recipes/maven.rb
+++ recipes/maven.rb
@@ -15,36 +15,36 @@ if node['artifact-deployer']['install_maven']
       repo_item = data_bag_item(repos_databag,repo)
       maven_repos.push repo_item
     end
+  rescue
+    Chef::Log.warn("Cannot find databag "+repos_databag+"; skipping Maven installation")
+  end
 
-    template  "#{m2_home}/conf/settings.xml" do
-      source  "settings.xml.erb"
+  template  "#{m2_home}/conf/settings.xml" do
+    source  "settings.xml.erb"
+    mode    0666
+    owner   "root"
+    group   "root"
+    variables(
+      :repos => maven_repos
+    )
+  end
+
+  if !master_password.empty?
+    directory  "/root/.m2" do
       mode    0666
       owner   "root"
       group   "root"
-      variables(
-        :repos => maven_repos
-      )
     end
 
-    if !master_password.empty?
-      directory  "/root/.m2" do
-        mode    0666
-        owner   "root"
-        group   "root"
-      end
-
-      template  "/root/.m2/settings-security.xml" do
-        source  "settings-security.xml.erb"
-        mode    0666
-        owner   "root"
-        group   "root"
-      end
+    template  "/root/.m2/settings-security.xml" do
+      source  "settings-security.xml.erb"
+      mode    0666
+      owner   "root"
+      group   "root"
     end
+  end
 
-    link "/usr/bin/mvn" do
-      to "/usr/local/maven/bin/mvn"
-    end
-  rescue
-    Chef::Log.warn("Cannot find databag "+repos_databag+"; skipping Maven installation")
+  link "/usr/bin/mvn" do
+    to "/usr/local/maven/bin/mvn"
   end
 end
