10ceeaca9e53a49e917653a2fb99401cc76017b4
Maurizio Pillitu <maoo@apache.org>
2015-04-17 11:32:49 +0200

    forcing python cookbook version

--- metadata.rb
+++ metadata.rb
@@ -10,11 +10,14 @@ supports          "ubuntu-12.04"
 supports          "centos-6.6"
 supports          "centos-7"
 
+# Resolved by Berkshelf, not present in Supermarket or forked
 depends "maven"
 depends "file"
-depends "python"
 
+# Community cookbooks
+depends "python", "~> 1.4.6"
+
+recipe 'artifacts', 'Installs artifacts'
+recipe 'awscli', 'Installs Python awscli util'
 recipe 'default', 'Invokes all other recipes'
 recipe 'maven', 'Installs Apache Maven'
-recipe 'awscli', 'Installs Python awscli util'
-recipe 'artifacts', 'Installs artifacts'
