09ce29ab33b2fafd710d34e9abbad213cc6b6693
Maurizio Pillitu <maoo@apache.org>
2015-04-21 10:42:15 +0200

    manual version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "maurizio@session.it"
 license           "Apache 2.0"
 description       "A Chef Cookbook that provides a simple way to download, unpack and configure artifacts"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.7"
+version           "0.8.8"
 
 supports          "ubuntu-12.04"
 supports          "centos-6.6"
