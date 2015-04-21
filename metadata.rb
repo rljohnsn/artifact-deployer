856254ea5498a4d66c9f0b2e64b3c7f9a2145b98
Maurizio Pillitu <maoo@apache.org>
2015-04-21 12:54:35 +0200

    bump to 0.8.10 version, using apt version that is compatible with salt cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "maurizio@session.it"
 license           "Apache 2.0"
 description       "A Chef Cookbook that provides a simple way to download, unpack and configure artifacts"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.9"
+version           "0.8.10"
 
 supports          "ubuntu-12.04"
 supports          "centos-6.6"
@@ -16,7 +16,8 @@ depends "file"
 
 # Community cookbooks
 depends "python", "~> 1.4.6"
-depends "apt", "~> 2.7.0"
+# depends "apt", "~> 2.7.0"
+depends 'apt', '~> 2.3.10'
 depends "ark", "~> 0.9.0"
 depends "resolver", "~> 1.2.0"
 depends "java", "~> 1.31.0"
