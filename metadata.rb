168f80cd7b9ef80726003bba1bdb58a497705876
Maurizio Pillitu <maoo@apache.org>
2015-07-10 15:29:30 +0200

    updating apt version to latest
    updating apt version to latest

--- metadata.rb
+++ metadata.rb
@@ -16,8 +16,8 @@ depends "file"
 
 # Community cookbooks
 depends "python", "~> 1.4.6"
-# depends "apt", "~> 2.7.0"
-depends 'apt', '~> 2.3.10'
+depends "apt", "~> 2.7.0"
+# depends 'apt', '~> 2.3.10'
 depends "ark", "~> 0.9.0"
 depends "resolver", "~> 1.2.0"
 depends "java", "~> 1.31.0"
