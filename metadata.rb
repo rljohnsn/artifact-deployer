ac6082d1ae4965653ab48de72c9cf6aa5629fcca
rljohnsn <johnsonrl@yahoo.com>
2015-09-17 10:13:42 -0700

    [jetty] - update third party cookbook kitchen driver to docker - fix cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -11,18 +11,18 @@ supports          "centos-6.6"
 supports          "centos-7"
 
 # Resolved by Berkshelf, not present in Supermarket or forked
-depends "maven", '~> 1.2.0'
+depends "maven"
 depends "file"
 
 # Community cookbooks
-depends "python", "~> 1.4.6"
-depends "apt", "~> 2.7.0"
+depends "python"
+depends "apt"
 # depends 'apt', '~> 2.3.10'
-depends "ark", "~> 0.9.0"
-depends "resolver", "~> 1.2.0"
-depends "java", "~> 1.31.0"
+depends "ark"
+depends "resolver"
+depends "java"
 
-recipe 'artifacts', 'Installs artifacts'
-recipe 'awscli', 'Installs Python awscli util'
-recipe 'default', 'Invokes all other recipes'
-recipe 'maven', 'Installs Apache Maven'
+#recipe 'artifacts', 'Installs artifacts'
+#recipe 'awscli', 'Installs Python awscli util'
+#recipe 'default', 'Invokes all other recipes'
+#recipe 'maven', 'Installs Apache Maven'
